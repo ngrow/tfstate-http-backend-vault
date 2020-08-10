9b0ed6a1fb92f6b12a39e3acbf98639bd62b1bb8
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2020-08-10 10:32:48 +0300

    Add more configurable logger
    We use github.com/hashicorp/go-hclog in order to reduce number of
    dependencies. So, we use hclog since vault is dependent internally on
    hclog.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ngrow/tfstate-http-backend-vault
+
+go 1.14
+
+require (
+	github.com/hashicorp/go-hclog v0.8.0
+	github.com/hashicorp/vault/api v1.0.4
+)
--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"encoding/base64"
+	"errors"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 
+	"github.com/hashicorp/go-hclog"
 	vault_api "github.com/hashicorp/vault/api"
 )
-
-const DEBUG = false
 
 // fileExists checks if a file exists and is not a directory before we
 // try using it to prevent further errors.
@@ -22,52 +21,66 @@ func fileExists(filename string) bool {
 	return !info.IsDir()
 }
 
-func main() {
-	secret_path := os.Getenv("TFSTATE_SECRET_PATH")
-	if secret_path == "" {
-		secret_path = "secret/data/tfstate"
-	}
+func newVaultClient() (*vault_api.Client, error) {
 	vault_client, err := vault_api.NewClient(nil)
 	if err != nil {
-		log.Fatal(err)
+		hclog.Default().Error("failed to create Vault API client", "err", err)
+		return nil, err
 	}
 	if _, ok := os.LookupEnv("VAULT_TOKEN"); !ok {
 		token_filename := os.Getenv("HOME") + "/.vault-token"
 		if fileExists(token_filename) {
-			token, err := ioutil.ReadFile(token_filename)
-			if err != nil {
-				log.Fatal("couldn't read token: ", err)
-			}
-			vault_client.SetToken(string(token))
-		} else {
-			log.Fatal("vault token is not specified")
+			var err = errors.New("there is such file: " + token_filename)
+			hclog.Default().Error("vault token is not specified", "err", err)
+			return nil, err
 		}
+		token, err := ioutil.ReadFile(token_filename)
+		if err != nil {
+			hclog.Default().Error("couldn't read token", "err", err)
+			return nil, err
+		}
+		vault_client.SetToken(string(token))
+	}
+	return vault_client, nil
+}
+
+func main() {
+	hclog.Default().SetLevel(hclog.Info)
+
+	secret_path := os.Getenv("TFSTATE_SECRET_PATH")
+	if secret_path == "" {
+		secret_path = "secret/data/tfstate"
+	}
+	hclog.Default().Info("tfstate stored at", "path", secret_path)
+
+	vault_client, err := newVaultClient()
+	if err != nil {
+		os.Exit(1)
 	}
 	vault := vault_client.Logical()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "POST":
 			body, err := ioutil.ReadAll(r.Body)
 			if err != nil {
 				w.WriteHeader(500)
-				log.Print("couldn't read request body: ", err)
+				hclog.Default().Info("couldn't read request body", "err", err)
 				return
 			}
 			encoded_body := base64.StdEncoding.EncodeToString(body)
 			_, err = vault.Write(secret_path, map[string]interface{}{"data": map[string]interface{}{"data": encoded_body}})
 			if err != nil {
 				w.WriteHeader(500)
-				log.Print("couldn't write tfstate: ", err)
+				hclog.Default().Info("couldn't write tfstate", "err", err)
 				return
 			}
-			if DEBUG {
-				log.Print("wrote state: ", string(body))
-			}
+			hclog.Default().Info("wrote state", "data", string(body))
 		case "GET":
 			secret, err := vault.Read(secret_path)
 			if err != nil {
 				w.WriteHeader(500)
-				log.Print("couldn't read tfstate: ", err)
+				hclog.Default().Info("couldn't read tfstate", "err", err)
 				return
 			}
 			if secret == nil {
@@ -80,18 +93,16 @@ func main() {
 			if data != nil {
 				decoded_data, err := base64.StdEncoding.DecodeString(data.(string))
 				if err != nil {
-					log.Print("couldn't decode base64: ", err)
+					hclog.Default().Info("couldn't decode base64", "err", err)
 					w.WriteHeader(500)
 					return
 				}
 				_, err = w.Write(decoded_data)
 				if err != nil {
-					log.Print("error writing response: ", err)
+					hclog.Default().Info("error writing response", "err", err)
 					return
 				}
-				if DEBUG {
-					log.Print("read state: ", decoded_data)
-				}
+				hclog.Default().Debug("read state", "data", string(decoded_data))
 			}
 		case "LOCK":
 			w.WriteHeader(405)
@@ -101,5 +112,8 @@ func main() {
 			w.WriteHeader(405)
 		}
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		hclog.Default().Error("failed to serve HTTP", "err", err)
+	}
 }
